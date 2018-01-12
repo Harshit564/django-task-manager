1fc1509ef03b9835423713ef3e0ccecf565353e3
jazzify <jm1218@hotmail.com>
2018-01-12 14:41:20 -0500

    Included django-allauth urls

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.conf.urls.i18n import i18n_patterns
 from .views import home, home_files
 
@@ -25,4 +25,5 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('accounts/', include('allauth.urls')),
 )
