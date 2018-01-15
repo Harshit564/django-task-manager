72f5dee121a7fef38e4c01b6e3055a8f8b9e9778
jazzify <jm1218@hotmail.com>
2018-01-15 11:22:43 -0500

    Created new app Manager

--- taskmanager/settings/base.py
+++ taskmanager/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'manager.apps.ManagerConfig',
     # All Auth:
     'allauth',
     'allauth.account',
